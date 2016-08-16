7a41654f112233bb85b94ddd0b2587dca4ea677e
Stéphane Bisinger <sbisinger@vendini.com>
2016-08-16 15:26:39 +0200

    Implement the find_repeated_substring function

--- lib/repeated_substring.rb
+++ lib/repeated_substring.rb
@@ -1,7 +1,18 @@
 class RepeatedSubstring
   def find_repeated_substring(string)
-    # TODO: implement the function
-    1 + 2
+    longest_word = 'NONE'
+    longest = string.length / 2
+    while longest_word == 'NONE' && longest > 1 do
+      (string.length - longest + 1).times do |n|
+        current_word = string[n..(longest + n - 1)]
+        unless string[longest + n, string.length].index(current_word).nil?
+          longest_word = current_word
+          break
+        end
+      end
+      longest -= 1
+    end
+    longest_word.strip
   end
 
   def find_repeated_substring_file(file_path)
